1c036ad42b65974b9edb4e90a4f9bc7ec7ded693
Miguel <migueljomartinez@gmail.com>
2023-08-22 17:41:28 -0500

    Initial Commit

---
+++ ObjectiveCTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveCTutorial
+//
+//  Created by Miguel Martinez on 22/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
