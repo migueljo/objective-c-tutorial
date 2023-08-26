c73aa425689e0c8492ff882e217aa62f305b8afa
Miguel <migueljomartinez@gmail.com>
2023-08-25 23:51:06 -0500

    Arrays part 2

--- ObjectiveCTutorial/main.m
+++ ObjectiveCTutorial/main.m
@@ -8,9 +8,13 @@ int main(int argc, const char * argv[]) {
         NSLog(@"First element: %@", officeSupplies[0]);
         
         BOOL containsBoard = [officeSupplies containsObject:@"Board"];
-        NSLog(@"Containes board? %d", containsBoard);
+        NSLog(@"Contains board? %d", containsBoard);
         BOOL containsPaper = [officeSupplies containsObject:@"Paper"];
-        NSLog(@"Containes paper? %d", containsPaper);
+        NSLog(@"Contains paper? %d", containsPaper);
+        
+        NSLog(@"Length: %d", (int)officeSupplies.count);
+        NSLog(@"Index of paper is: %d", (int)[officeSupplies indexOfObject:@"Paper"]);
+        NSLog(@"Index of pencils is: %d", (int)[officeSupplies indexOfObject:@"Pencils"]);
     }
     return 0;
 }
