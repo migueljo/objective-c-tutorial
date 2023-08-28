4911d7e26155214d8110ede5f39bf0629aa9c811
Miguel <migueljomartinez@gmail.com>
2023-08-27 23:43:05 -0500

    Archiving

--- ObjectiveCTutorial/main.m
+++ ObjectiveCTutorial/main.m
@@ -3,39 +3,9 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSArray *officeSupplies = @[@"Paper", @"Pencils"];
-        NSLog(@"Office supplies: %@", officeSupplies);
-        NSLog(@"First element: %@", officeSupplies[0]);
-        
-        BOOL containsBoard = [officeSupplies containsObject:@"Board"];
-        NSLog(@"Contains board? %d", containsBoard);
-        BOOL containsPaper = [officeSupplies containsObject:@"Paper"];
-        NSLog(@"Contains paper? %d", containsPaper);
-        
-        NSLog(@"Length: %d", (int)officeSupplies.count);
-        NSLog(@"Index of paper is: %d", (int)[officeSupplies indexOfObject:@"Paper"]);
-        NSLog(@"Index of pencils is: %d", (int)[officeSupplies indexOfObject:@"Pencils"]);
-        
-        NSMutableArray *heroes = [NSMutableArray arrayWithCapacity:5];
-        [heroes addObject:@"Batman"];
-        [heroes addObject:@"Spider man"];
-        [heroes addObject:@"Super man"];
-        [heroes addObject:@"Iron man"];
-        [heroes addObject:@"Wonder woman"];
-        [heroes addObject:@"Hulk"];
-        [heroes insertObject:@"Black panther" atIndex:2];
-        
-        NSLog(@"%@", heroes);
-        
-        [heroes removeObject:@"Hulk"];
-        NSLog(@"%@", [heroes objectAtIndex:3]);
-        [heroes removeObjectAtIndex:3];
-        
-        NSLog(@"%@", heroes);
-        
-        for (int i = 0; i < heroes.count; i++) {
-            NSLog(@"Hero at index: %d is %@", i, heroes[i]);
-        }
+        // Custom objects ****
+        NSLog(@"Custom objects");
+        // **** Custom objects
     }
     return 0;
 }
--- ObjectiveCTutorial/objective-c-archive.m
+++ ObjectiveCTutorial/objective-c-archive.m
@@ -47,4 +47,40 @@ void archive() {
     NSLog(@"Grocery list: %@", groceryList);
     [groceryList insertString:@", Lemon, Tomatoes" atIndex:[groceryList length]];
     NSLog(@"Grocery list: %@", groceryList);
+    
+    // Arrays ****
+    NSArray *officeSupplies = @[@"Paper", @"Pencils"];
+    NSLog(@"Office supplies: %@", officeSupplies);
+    NSLog(@"First element: %@", officeSupplies[0]);
+    
+    BOOL containsBoard = [officeSupplies containsObject:@"Board"];
+    NSLog(@"Contains board? %d", containsBoard);
+    BOOL containsPaper = [officeSupplies containsObject:@"Paper"];
+    NSLog(@"Contains paper? %d", containsPaper);
+    
+    NSLog(@"Length: %d", (int)officeSupplies.count);
+    NSLog(@"Index of paper is: %d", (int)[officeSupplies indexOfObject:@"Paper"]);
+    NSLog(@"Index of pencils is: %d", (int)[officeSupplies indexOfObject:@"Pencils"]);
+    
+    NSMutableArray *heroes = [NSMutableArray arrayWithCapacity:5];
+    [heroes addObject:@"Batman"];
+    [heroes addObject:@"Spider man"];
+    [heroes addObject:@"Super man"];
+    [heroes addObject:@"Iron man"];
+    [heroes addObject:@"Wonder woman"];
+    [heroes addObject:@"Hulk"];
+    [heroes insertObject:@"Black panther" atIndex:2];
+    
+    NSLog(@"%@", heroes);
+    
+    [heroes removeObject:@"Hulk"];
+    NSLog(@"%@", [heroes objectAtIndex:3]);
+    [heroes removeObjectAtIndex:3];
+    
+    NSLog(@"%@", heroes);
+    
+    for (int i = 0; i < heroes.count; i++) {
+        NSLog(@"Hero at index: %d is %@", i, heroes[i]);
+    }
+    // **** Arrays
 }
