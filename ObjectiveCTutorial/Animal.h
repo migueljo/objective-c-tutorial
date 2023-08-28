3a8f17a256c92b4acd96f23599d2e3caba449cdd
Miguel <migueljomartinez@gmail.com>
2023-08-28 00:08:13 -0500

    Writing animal interface

--- ObjectiveCTutorial/Animal.h
+++ ObjectiveCTutorial/Animal.h
@@ -4,6 +4,23 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Animal : NSObject
 
+// Read only property
+@property NSString *name;
+@property NSString *favoriteFood;
+@property NSString *sound;
+
+@property float weight;
+
+-(instancetype) initWithName:(NSString *) defaultName;
+
+-(void) getInfo;
+
+-(float) weightInKg:(float) weightInLbs;
+
+-(NSString *) talkToMe:(NSString *) name appendix:(NSString *) appendix;
+
+-(int) getSum: (int) number1 nextNumber:(int) number2;
+
 @end
 
 NS_ASSUME_NONNULL_END
