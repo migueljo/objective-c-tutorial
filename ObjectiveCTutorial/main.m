a257bfa051273d35db0e1fede79c9fc920c12d14
Miguel <migueljomartinez@gmail.com>
2023-08-28 23:45:03 -0500

    Playing with the instance object

--- ObjectiveCTutorial/main.m
+++ ObjectiveCTutorial/main.m
@@ -9,6 +9,12 @@ int main(int argc, const char * argv[]) {
         Animal *dog = [[Animal alloc] init];
         [dog getInfo];
         
+        // the "name" function in this case is auto generated for us
+        NSLog(@"The dog's name is: %@", [dog name]);
+        NSLog(@"2. The dog's name is: %@", dog.name);
+        // the "setName" function in this case is auto generated for us too
+        [dog setName:@"Puche"];
+        NSLog(@"The dog's name is now: %@", [dog name]);
         // **** Custom objects
     }
     return 0;
