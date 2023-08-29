58a9d6d4bfab979cd45dd8cf80a221b30f1a208e
Miguel <migueljomartinez@gmail.com>
2023-08-28 23:30:51 -0500

    Implementing talkToMe

--- ObjectiveCTutorial/Animal.m
+++ ObjectiveCTutorial/Animal.m
@@ -18,6 +18,15 @@
     return weightInLbs * 2;
 }
 
+- (NSString *) talkToMe:(NSString *)name appendix:(NSString *)appendix {
+    NSMutableString *greetings = [NSMutableString stringWithString:@"Hello "];
+    [greetings appendString:name];
+    [greetings appendString:@" "];
+    [greetings appendString:appendix];
+    
+    return [greetings copy];
+}
+
 - (int) getSum:(int)number1 nextNumber:(int)number2 {
     return number1 + number2;
 }
