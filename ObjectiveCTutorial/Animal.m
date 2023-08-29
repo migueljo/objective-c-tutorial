ddd7961e017d11a701d05769a757c7da7b5f6fa7
Miguel <migueljomartinez@gmail.com>
2023-08-28 23:23:45 -0500

    Implementing Animal methods part 1

--- ObjectiveCTutorial/Animal.m
+++ ObjectiveCTutorial/Animal.m
@@ -2,4 +2,24 @@
 
 @implementation Animal
 
+- (instancetype) init {
+    self = [super init];
+    if (self) {
+        // 
+    }
+    return self;
+}
+
+- (void) getInfo {
+    printf("Info \n");
+}
+
+- (float) weightInKg:(float)weightInLbs {
+    return weightInLbs * 2;
+}
+
+- (int) getSum:(int)number1 nextNumber:(int)number2 {
+    return number1 + number2;
+}
+
 @end
