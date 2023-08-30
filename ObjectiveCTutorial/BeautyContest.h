e4309faaf692207f657158603898602207cf0a11
Miguel <migueljomartinez@gmail.com>
2023-08-29 23:58:32 -0500

    Creating protocol file

---
+++ ObjectiveCTutorial/BeautyContest.h
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/*
+ A protocol is a bunch of properties and methods that any class will be able to implement
+ */
+
+@protocol BeautyContest <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
